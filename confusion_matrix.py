499ed8aa7b125f91e54adfabcc39f4047eadd933
Prajwal Nayaka <prajwalnayakat@gmail.com>
2026-02-05 14:14:50 +0530

    Create confusion_matrix

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -1,3 +1,9 @@
 from train_isolation_forest import cm
 import matplotlib.pyplot as plt
 import seaborn as sns
+plt.style.use('fivethirtyeight')
+plt.figure(figsize=(10, 8))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',xticklabels=['Normal', 'Attack'], yticklabels=['Normal', 'Attack'])
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.show()
