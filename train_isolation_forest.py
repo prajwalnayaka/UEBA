39c90288bcfb913b0463f6de82c6ba863b88e7be
Prajwal Nayaka T <prajwalnayakat@gmail.com>
2026-02-15 15:12:09 +0530

    Update train_isolation_forest.py

--- train_isolation_forest.py
+++ train_isolation_forest.py
@@ -1,22 +1,23 @@
 import pandas as pd
 from sklearn.ensemble import IsolationForest
 from sklearn.metrics import classification_report, confusion_matrix
+
 ds = pd.read_csv("game_admin_derived.csv")
-features = ['hour_of_day', 'actions_per_min', 'is_rare_ip']
+features = ["hour_of_day", "actions_per_min", "is_rare_ip"]
 X = ds[features]
 model = IsolationForest(contamination=0.04, random_state=42)
 model.fit(X)
 preds = model.predict(X)
 
 # If pred is -1 (anomaly), we call it 1 (attack). Otherwise 0.
-ds['pred_label'] = [1 if x == -1 else 0 for x in preds]
+ds["pred_label"] = [1 if x == -1 else 0 for x in preds]
 
 
 print("\n--- MODEL PERFORMANCE ---")
-y_true = ds['is_attack']
-y_pred = ds['pred_label']
+y_true = ds["is_attack"]
+y_pred = ds["pred_label"]
 
-print(classification_report(y_true, y_pred, target_names=['Normal', 'Rogue']))
+print(classification_report(y_true, y_pred, target_names=["Normal", "Rogue"]))
 
 # Confusion Matrix
 cm = confusion_matrix(y_true, y_pred)
@@ -27,5 +28,5 @@ print(f"False Negatives (Attack missed): {cm[1][0]}")
 print(f"True Positives (Attack caught): {cm[1][1]}")
 
 # Save results as before
-ds['anomaly_score'] = preds 
+ds["anomaly_score"] = preds
 ds.to_csv("game_admin_results.csv", index=False)
