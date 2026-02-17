3816bb1fdd2e5db0c07a511aec075320a29dd568
Prajwal Nayaka <prajwalnayakat@gmail.com>
2026-02-17 19:08:31 +0530

    Create train_logistic.py

---
+++ train_logistic.py
@@ -0,0 +1,23 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+import pandas as pd
+
+ds=pd.read_csv("game_admin_derived.csv")
+X = ds[["hour_of_day", "actions_per_min", "is_rare_ip"]]
+y = ds["is_attack"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=1)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print("\n--- Logistic Regression Performance ---")
+print(classification_report(y_test, y_pred))
+cm=confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(f"True Negatives (Normal detected as Normal): {cm[0][0]}")
+print(f"False Positives (Normal flagged as Attack): {cm[0][1]}")
+print(f"False Negatives (Attack missed): {cm[1][0]}")
+print(f"True Positives (Attack caught): {cm[1][1]}")
