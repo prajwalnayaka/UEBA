b32b252be428ee625775299a2af847cd429851a6
Prajwal Nayaka <prajwalnayakat@gmail.com>
2026-02-21 15:56:52 +0530

    Update train_logistic_regression.py

--- train_logistic_regression.py
+++ train_logistic_regression.py
@@ -2,10 +2,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 ds = pd.read_csv("game_admin_derived.csv")
 X = ds[["hour_of_day", "actions_per_min", "is_rare_ip"]]
 y = ds["is_attack"]
+class_names=['Normal','Malicious']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
@@ -16,8 +19,7 @@ y_pred = model.predict(X_test)
 print("\n--- Logistic Regression Performance ---")
 print(classification_report(y_test, y_pred,target_names=['Normal','Rogue']))
 cm = confusion_matrix(y_test, y_pred)
-print("Confusion Matrix:")
-print(f"True Negatives (Normal detected as Normal): {cm[0][0]}")
-print(f"False Positives (Normal flagged as Attack): {cm[0][1]}")
-print(f"False Negatives (Attack missed): {cm[1][0]}")
-print(f"True Positives (Attack caught): {cm[1][1]}")
+plt.figure(figsize=(8, 8))
+sns.heatmap(cm, annot=True,fmt="d",cmap="YlGnBu",xticklabels=class_names,yticklabels=class_names)
+plt.savefig("Performance/LR_Confusion_Matrix.png",dpi=600)
+plt.show()
